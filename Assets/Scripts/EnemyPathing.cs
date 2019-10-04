f966f546a88b89e9daa8a8167d625f2e73c27acf
corymaklin <corymaklin@gmail.com>
2019-10-04 10:45:49 -0400

    added enemy waves

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int index = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        waypoints = waveConfig.GetWayPoints();
+        transform.position = waypoints[index].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (index < waypoints.Count)
+        {
+            var targetPosition = waypoints[index].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+            {
+                index++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfig> waveConfigs;
+    int startingWave = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var currentWave = waveConfigs[startingWave];
+
+        StartCoroutine(SpawnAllEnemiesInWave(currentWave));
+    }
+
+    private IEnumerator SpawnAllEnemiesInWave(WaveConfig waveConfig)
+    {
+        Instantiate(waveConfig.GetEnemyPrefab(), waveConfig.GetWayPoints()[0].transform.position, Quaternion.identity);
+
+        yield return new WaitForSeconds(waveConfig.GetTimeBetweenSpawns());
+    }
+}
